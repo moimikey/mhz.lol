b4d459310721511dc917ca76a906b7ef28e881db
moimikey <mshertzberg@gmail.com>
2024-11-30 12:04:32 -0500

    update meta

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,6 +7,7 @@ import {
   FaInstagram,
   FaRss,
   FaLinkedinIn,
+  FaBluesky,
 } from "react-icons/fa6";
 import { TbMailFilled } from "react-icons/tb";
 import { metaData, socialLinks } from "app/config";
@@ -24,9 +25,8 @@ function SocialLink({ href, icon: Icon }) {
 function SocialLinks() {
   return (
     <div className="flex text-lg gap-3.5 float-right transition-opacity duration-300 hover:opacity-90">
-      <SocialLink href={socialLinks.twitter} icon={FaXTwitter} />
+      <SocialLink href={socialLinks.bluesky} icon={FaBluesky} />
       <SocialLink href={socialLinks.github} icon={FaGithub} />
-      <SocialLink href={socialLinks.instagram} icon={FaInstagram} />
       <SocialLink href={socialLinks.linkedin} icon={FaLinkedinIn} />
       <SocialLink href={socialLinks.email} icon={TbMailFilled} />
       <a href="/rss.xml" target="_self">
@@ -42,7 +42,7 @@ export default function Footer() {
       <time>© {YEAR}</time>{" "}
       <a
         className="no-underline"
-        href={socialLinks.twitter}
+        href={socialLinks.bluesky}
         target="_blank"
         rel="noopener noreferrer"
       >
--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,15 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://mhz.lol",
+  title: "@moimikey",
+  name: "Mchl Hzbrg",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    ":)",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  bluesky: "https://bsky.app/profile/mhz.lol",
+  github: "https://github.com/moimikey",
+  linkedin: "https://www.linkedin.com/in/mshertzberg",
+  email: "mailto:hello@mhz.lol",
 };
