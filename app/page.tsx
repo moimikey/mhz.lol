6ab785db13bbbe2cbe5b1f872172f166895403c8
moimikey <mshertzberg@gmail.com>
2025-02-01 11:00:08 -0500

    fix: type issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
 import Image from "next/image";
-import { socialLinks } from "./config";
-import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <section>
-      <a href={socialLinks.bluesky} target="_blank">
-        <Image
-          src="/profile.png"
-          alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          unoptimized
-          width={160}
-          height={160}
-          priority
-        />
-      </a>
+      <Image
+        src="/profile.png"
+        alt="Profile photo"
+        className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+        unoptimized
+        width={160}
+        height={160}
+        priority
+      />
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
         Hello Universe!
