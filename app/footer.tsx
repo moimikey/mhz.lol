b4725c5da86f5182c90b4f685fdb43dcde015834
moimikey <mshertzberg@gmail.com>
2025-07-26 19:05:08 -0400

    feat: update template with personalized touch

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,10 +70,13 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/moimikey/mhz.lol" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2024 Nim.</span>
-            <span>Built with Motion-Primitives.</span>
+            <span>© 2025 Michael Scott Hertzberg</span>
+            <span>
+              Built with Next.js 15, React 19, Tailwind CSS v4, and
+              Motion-Primitives. with Nim
+            </span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,16 +12,17 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://nim-fawn.vercel.app/'),
+  metadataBase: new URL('https://mhzlol.vercel.app/'),
   alternates: {
-    canonical: '/'
+    canonical: '/',
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Michael Scott Hertzberg | mhz.lol',
+    template: '%s | mhz.lol',
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
-};
+  description:
+    "Michael Scott Hertzberg's personal website built with Next.js 15, React 19 and Motion-Primitives.",
+}
 
 const geist = Geist({
   variable: '--font-geist',
